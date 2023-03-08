177aae80fbec76e9714b3d94e449f05f1d9df9f9
dupegui334 <davidupegui334@hotmail.com>
2023-03-07 20:34:53 -0500

    Improved the API get, so user can get info of planet

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    url = 'https://api.le-systeme-solaire.net/rest.php/bodies?data=id%2CisPlanet&rowData=false'
-    get_solar = requests.get(url)
-    print(get_solar.content)
-    return render_template('index.html')
+
+    if request.method == 'GET':
+        return render_template('index.html')
+    
+    if request.form['search']:
+        
+        url = 'https://api.le-systeme-solaire.net/rest.php/bodies/' + request.form['search']
+        get_solar = requests.get(url)
+        print(get_solar.content)
+        get_solar_json = get_solar.json()
+        return render_template('index.html', data = get_solar_json)
+    
+    else:
+        return render_template('index.html')
+
 
 # @app.route('/how are you')
 # def hello():

2724835edf4475abe3cecbd6f11336b159059e0f
David Upegui <davidupegui334@hotmail.com>
2023-05-09 19:38:53 -0500

    Set the key to access ec2 and SG

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -5,5 +5,18 @@ resource "aws_instance" "ec2-webapp" {
     Name = "webserver-app-tf"
     Description = "Flask app server on Ubuntu"
   }
+  key_name = data.aws_key_pair.web-app-key.key_name
+  vpc_security_group_ids = [data.aws_security_group.app-sg.id]
 }
 
+data "aws_key_pair" "web-app-key" {
+  key_name = "centos-ec2"
+}
+
+data "aws_security_group" "app-sg" {
+  id = "sg-07355b7f88fa76532"
+}
+
+output publicip {
+  value = aws_instance.ec2-webapp.public_ip
+}

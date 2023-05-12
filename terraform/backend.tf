68fb88fe63cb58f79e2d0864600d7105915c0426
David Upegui <davidupegui334@hotmail.com>
2023-05-11 21:09:49 -0500

    Moved terraform.lock to AWS dynamodb table

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,3 +5,13 @@ terraform {
     region = "us-east-1"
   }
 }
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "web-app-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}

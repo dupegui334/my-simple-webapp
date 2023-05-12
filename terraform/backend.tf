53bb78d32808a23a9632d9bb37c251fb153d71c4
David Upegui <davidupegui334@hotmail.com>
2023-05-11 21:16:40 -0500

    Set deletion protection to dynamodb

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -11,6 +11,7 @@ resource "aws_dynamodb_table" "terraform_locks" {
   name         = "web-app-lock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+  deletion_protection_enabled = true
   attribute {
     name = "LockID"
     type = "S"
